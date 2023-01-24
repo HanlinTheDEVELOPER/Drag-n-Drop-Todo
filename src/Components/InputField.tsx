607bdae454ab56e10950f23eaa9bdb8e7a3f61a1
HanlinTheDEVELOPER <110489348+HanlinTheDEVELOPER@users.noreply.github.com>
2023-01-24 22:00:38 +0630

    add or delete todos

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const InputField: React.FC = () => {
-  return <div>InputField</div>;
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="Enter Task Name"
+        className="input__box"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit" className="input_submit">
+        Go
+      </button>
+    </form>
+  );
 };
 
 export default InputField;
---
+++ src/Components/SingleTodo.tsx
@@ -0,0 +1,46 @@
+import { Todo } from "../Types/interface";
+import { AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import { FaTrash } from "react-icons/fa";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  return (
+    <form className="todos__single">
+      {todo.isDone ? (
+        <s className="todos__single--text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single--text">{todo.todo}</span>
+      )}
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <FaTrash />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
